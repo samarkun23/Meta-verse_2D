19dcfd2f57a387f8b141e6d20af92c0f0fb11027
Samarkun <samarkun4@gmail.com>
2025-10-09 22:30:50 +0530

    space inforamtion code basically here you create a elements and map

--- tests/index.test.js
+++ tests/index.test.js
@@ -195,6 +195,85 @@ describe('user avatar information', () => {
 
 })
 
+describe("Space information", () => {
+  let mapId;
+  let element1Id;
+  let element2Id;
+  let token;
+  let userId;
 
+  beforeAll(async () => {
+    const username = `samar-${Math.random()}`
+    const password = "123456"
+
+    const signupResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: 'admin'
+    });
+
+    userId = signupResponse.data.userId;
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password
+    })
+
+    token = response.data.token;
+
+    const element1 = await axios.post(`${BACKEND_URL}/api/v1/admin/element`, {
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+      "width": 1,
+      "height": 1,
+      "static": true
+
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    const element2 = await axios.post(`${BACKEND_URL}/api/v1/admin/element`, {
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+      "width": 1,
+      "height": 1,
+      "static": true
+
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    element1Id = element1.id;
+    element2Id = element2.id;
+
+    const map = await axios.post(`${BACKEND_URL}/api/v1/admin/map`, {
+
+      "thumbnail": "https://thumbnail.com/a.png",
+      "dimensions": "100x200",
+      "name": "100 person interview room",
+      "defaultElements": [{
+        elementId: element1Id,
+        x: 20,
+        y: 20
+      }, {
+        elementId: element1Id,
+        x: 18,
+        y: 20
+      }, {
+        elementId: element2Id,
+        x: 19,
+        y: 20
+      }]
+    }, {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+    mapId = map.id
+
+  });
+})
 
 
