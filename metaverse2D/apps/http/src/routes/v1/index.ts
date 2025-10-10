9a6e2bc4b63368d7c1e3396d90822ac176cc10b4
Samarkun <samarkun4@gmail.com>
2025-10-10 18:22:54 +0530

    skeleteons for all the routes

--- metaverse2D/apps/http/src/routes/v1/index.ts
+++ metaverse2D/apps/http/src/routes/v1/index.ts
@@ -1,3 +1,26 @@
 import { Router } from "express"
+import { userRouter } from "./user.js";
+import { spaceRouter } from "./space.js";
+import { adminRouter } from "./admin.js";
 
-const router = Router();
+export const router = Router();
+
+router.post("/signup", (req,res) => {
+    res.json({
+        message: "signup"
+    })
+})
+
+router.post("/signin", (req,res) => {
+    res.json({
+        message: "signin"
+    })
+})
+
+router.get("/elements", (req,res) => {})
+
+router.get("/avatars", (req,res) => {})
+
+router.use("/user", userRouter)
+router.use("/space", spaceRouter)
+router.use("/admin", adminRouter)
---
+++ metaverse2D/apps/http/src/routes/v1/user.ts
@@ -0,0 +1,13 @@
+import { application, Router } from "express";
+
+
+export const userRouter = Router();
+
+
+userRouter.post("/metadata", (req,res) => {
+
+})
+
+userRouter.get("/metadata/bulk",(req,res) => {
+
+})
