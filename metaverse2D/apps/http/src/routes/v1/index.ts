7abde093685ed0432e1ce8dd08e178f3fb0312a5
samarkun <samarkun4@gmail.com>
2025-11-06 13:27:00 -0500

    signIn route made

--- metaverse2D/apps/http/src/routes/v1/index.ts
+++ metaverse2D/apps/http/src/routes/v1/index.ts
@@ -2,8 +2,11 @@ import { Router } from "express"
 import { userRouter } from "./user.js";
 import { spaceRouter } from "./space.js";
 import { adminRouter } from "./admin.js";
-import { SignUpSchema } from "src/types/index.js";
+import { SignInSchema, SignUpSchema } from "src/types/index.js";
+import bcrypt from "bcrypt"
 import client from "@repo/db"
+import jwt from "jsonwebtoken"
+import { config } from "src/config.js";
 
 export const router = Router();
 
@@ -14,11 +17,13 @@ router.post("/signup", async (req,res) => {
         return res.status(400).json({message: "SignUp parse failed"})
     }
 
+    const hashedPassword = await bcrypt.hash(parseData.data.password, 10);
+
     try {
         const user = await client.user.create({
             data:{
-                usename: parseData.data.username,
-                password: parseData.data.password,
+                username: parseData.data.username,
+                password: hashedPassword,
                 role: parseData.data.type === "admin" ? "Admin" : "User",
             }
         }) 
@@ -31,11 +36,44 @@ router.post("/signup", async (req,res) => {
 
 })
 
-router.post("/signin", (req,res) => {
+router.post("/signin", async(req,res) => {
+    const parseData = SignInSchema.safeParse(req.body);
     
-    res.json({
-        message: "signin"
-    })
+    if(!parseData.success){
+        return res.status(403).json({message: "Invalid Credentials"})
+    }
+
+    try {
+        const user = await client.user.findUnique({
+            where: {
+                username: parseData.data.username
+            }
+        })
+
+        if(!user){
+            return res.status(403).json({messsage: "User not found"})
+        }
+
+        const validatePassword = await bcrypt.compare(parseData.data.password , user.password)
+        if(!validatePassword){
+            return res.status(403).json({message: "Invalid Password"});
+        }
+
+       const token = jwt.sign({
+            userId: user.id,
+            role: user.role
+       }, config.jwtSecret , { expiresIn: "23h"});
+
+        res.json({
+            token ,
+            message: "User signIn"
+        })
+
+    } catch (e) {
+        res.status(500).json({
+            message: "Error while SignIn" + e
+        })
+    }
 })
 
 router.get("/elements", (req,res) => {})
