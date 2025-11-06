6fad72d1a82ea5a49b637441b3822f17d1ef4e9f
samarkun <samarkun4@gmail.com>
2025-11-06 09:17:00 -0500

    signup endpoint

--- metaverse2D/apps/http/src/index.ts
+++ metaverse2D/apps/http/src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import { router } from "./routes/v1/index.js";
 import client from "@repo/db";
 
-const app = express(); 
+const app = express();
 
 app.use("/api/v1", router)
+app.use(express.json);
 
 
 
--- metaverse2D/apps/http/src/routes/v1/index.ts
+++ metaverse2D/apps/http/src/routes/v1/index.ts
@@ -2,17 +2,37 @@ import { Router } from "express"
 import { userRouter } from "./user.js";
 import { spaceRouter } from "./space.js";
 import { adminRouter } from "./admin.js";
+import { SignUpSchema } from "src/types/index.js";
+import client from "@repo/db"
 
 export const router = Router();
 
-router.post("/signup", (req,res) => {
+router.post("/signup", async (req,res) => {
     //check the user 
-    res.json({
-        message: "signup"
-    })
+    const parseData = SignUpSchema.safeParse(req.body);
+    if(!parseData.success){
+        return res.status(400).json({message: "SignUp parse failed"})
+    }
+
+    try {
+        const user = await client.user.create({
+            data:{
+                usename: parseData.data.username,
+                password: parseData.data.password,
+                role: parseData.data.type === "admin" ? "Admin" : "User",
+            }
+        }) 
+        res.json({
+            userId: user.id
+        })
+    } catch (e) {
+        res.status(400).json({message: "User already exists"})     
+    }
+
 })
 
 router.post("/signin", (req,res) => {
+    
     res.json({
         message: "signin"
     })
