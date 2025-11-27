ada4b113638a85cdf7924f42ab6cdf54b4e62882
Turbobot <turbobot@vercel.com>
2025-11-27 12:20:13 -0500

    feat(create-turbo): apply official-starter transform

--- metaverse2D/apps/http/src/config.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const config = {
-    jwtSecret: process.env.JWT_SECRET as string,
-    port: process.env.PORT || 3000,
-};
--- metaverse2D/packages/db/src/index.ts
+++
@@ -1,6 +0,0 @@
-import type { PrismaClient } from "@prisma/client";
-import { PrismaClient as PrismaClientClass } from "@prisma/client";
-
-const client: PrismaClient = new PrismaClientClass();
-
-export default client;
